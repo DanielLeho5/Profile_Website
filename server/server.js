a763422f69547fa337c53e29e9b1c22fd4a39d07
Daniel <lehoczki.dani5@gmail.com>
2026-08-11 15:54:28 +0200

    api update

--- server/server.js
+++ server/server.js
@@ -10,25 +10,23 @@ app.use(express.json())
 app.use(cors())
 
 const router = express.Router()
-app.use(
-    router.post("/api/mail", async (req, res) => {
-        try {
-            const {name, email, phone, text} = req.body || {}
-            if (!name || !email || !text) {return res.status(400).json({success: false, message: "Name, email and text are mandatory!"})}
+app.post("/api/mail", async (req, res) => {
+    try {
+        const {name, email, phone, text} = req.body || {}
+        if (!name || !email || !text) {return res.status(400).json({success: false, message: "Name, email and text are mandatory!"})}
 
-            await transporter.sendMail(mailOptions(
-                email,
-                "### Inquiry from profile website! ###",
-                `Name: ${name}\nEmail: ${email}\nPhone: ${phone || "- Not provided"}\n\n${text}`
-            ))
+        await transporter.sendMail(mailOptions(
+            email,
+            "### Inquiry from profile website! ###",
+            `Name: ${name}\nEmail: ${email}\nPhone: ${phone || "- Not provided"}\n\n${text}`
+        ))
 
-            return res.status(200).json({success: true, message: "Email sent successfully!"})
-        } catch (error) {
-            console.log(error)
-            return (res.status(500).json({success: false, message: "Something went wrong!", error: error.message}))
-        }
-    })
-)
+        return res.status(200).json({success: true, message: "Email sent successfully!"})
+    } catch (error) {
+        console.log(error)
+        return (res.status(500).json({success: false, message: "Something went wrong!", error: error.message}))
+    }
+})
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port ${PORT}!`);
